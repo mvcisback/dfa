9d7ad4bada8f6e224e005415d37cfc3565d7649a
Marcell Vazquez-Chanlatte <mvc@linux.com>
2019-07-05 19:42:27 -0700

    enable using methods from another start state.

--- dfa/dfa.py
+++ dfa/dfa.py
@@ -21,8 +21,8 @@ class DFA:
     )
     outputs: Alphabet = attr.ib(converter=frozenset, default={True, False})
 
-    def trace(self, word):
-        state = self.start
+    def trace(self, word, *, start=None):
+        state = self.start if start is None else start
         yield state
 
         for char in word:
@@ -30,16 +30,16 @@ class DFA:
             state = self._transition(state, char)
             yield state
 
-    def transition(self, word):
-        return fn.last(self.trace(word))
+    def transition(self, word, *, start=None):
+        return fn.last(self.trace(word, start=start))
 
-    def label(self, word):
-        output = self._label(self.transition(word))
+    def label(self, word, *, start=None):
+        output = self._label(self.transition(word, start=start))
         assert (self.outputs is None) or (output in self.outputs)
         return output
 
-    def transduce(self, word):
-        return tuple(map(self._label, self.trace(word)))[:-1]
+    def transduce(self, word, *, start=None):
+        return tuple(map(self._label, self.trace(word, start=start)))[:-1]
 
     @fn.memoize()
     def states(self):
