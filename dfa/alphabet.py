2d4e45fb5eabb39ee206852a5c62d3ba2be1d1f8
Marcell Vazquez-Chanlatte <mvc@linux.com>
2020-03-10 11:15:10 -0700

    Introduce exponential alphabet.

--- dfa/alphabet.py
+++ dfa/alphabet.py
@@ -6,15 +6,9 @@ from typing import Hashable, FrozenSet, Union
 
 import attr
 
-Letter = Hashable
 
-
-def _freeze(alphabet):
-    if alphabet is None or isinstance(alphabet, SupAlphabet):
-        return SupAlphabet()
-    elif isinstance(alphabet, (ProductAlphabet, ExplicitAlphabet)):
-        return alphabet
-    return ExplicitAlphabet(alphabet)
+def _instances_of(kind, *args):
+    return all(isinstance(x, kind) for x in args)
 
 
 def _lt_alphabet(left: Alphabet, right: Alphabet) -> bool:
@@ -23,9 +17,49 @@ def _lt_alphabet(left: Alphabet, right: Alphabet) -> bool:
         return False
     elif isinstance(right, SupAlphabet):
         return True
-    elif all(isinstance(x, ProductAlphabet) for x in [left, right]):
+    elif _instances_of(ProductAlphabet, left, right):
         return (left.left < right.left) and (left.right < right.right)
+    elif _instances_of(ExponentialAlphabet, left, right):
+        return left.dim == right.dim and left.base < right.base
+
     return set(left) < set(right)
+
+
+def _freeze(alphabet):
+    if alphabet is None or isinstance(alphabet, SupAlphabet):
+        return SupAlphabet()
+    elif isinstance(alphabet, Alphabet.__args__):
+        return alphabet
+    return ExplicitAlphabet(alphabet)
+
+
+Letter = Hashable
+
+
+@attr.s(frozen=True, auto_attribs=True, eq=False)
+@total_ordering
+class ExplicitAlphabet:
+    """Alphabet defined by a set."""
+    chars: FrozenSet[Letter] = attr.ib(converter=frozenset)
+
+    __lt__ = _lt_alphabet
+
+    def __hash__(self):
+        return hash(self.chars)
+
+    def __eq__(self, other):
+        if isinstance(other, ExplicitAlphabet):
+            return self.chars == other.chars
+        return set(self) == set(other)
+
+    def __iter__(self):
+        return iter(self.chars)
+
+    def __len__(self):
+        return len(self.chars)
+
+    def __contains__(self, elem):
+        return elem in self.chars
 
 
 @attr.s(frozen=True, auto_attribs=True, order=False)
@@ -71,29 +105,38 @@ class ProductAlphabet:
         return len(self.left) * len(self.right)
 
 
-@attr.s(frozen=True, auto_attribs=True, eq=False)
+@attr.s(frozen=True, auto_attribs=True, eq=False, repr=False)
 @total_ordering
-class ExplicitAlphabet:
-    chars: FrozenSet[Letter] = attr.ib(converter=frozenset)
+class ExponentialAlphabet:
+    """Product of base alphabet with itself dim times."""
+    base: Alphabet = attr.ib(converter=_freeze)
+    dim: int = attr.ib(default=1)
+
+    @dim.validator
+    def positive(self, _, value):
+        assert value >= 0
 
     __lt__ = _lt_alphabet
 
     def __hash__(self):
-        return hash(self.chars)
-
-    def __eq__(self, other):
-        if isinstance(other, ExplicitAlphabet):
-            return self.chars == other.chars
-        return set(self) == set(other)
-
-    def __iter__(self):
-        return iter(self.chars)
-
-    def __len__(self):
-        return len(self.chars)
+        return hash((self.base, self.dim))
 
     def __contains__(self, elem):
-        return elem in self.chars
+        for i, e in enumerate(elem):         # Allowing elem to be a generator.
+            if (i >= self.dim) or (e not in self.base):
+                return False
+        return i == self.dim
+
+    def __repr__(self):
+        return "{self.base} ^ {self.dim}"
+
+    def __len__(self):
+        return self.dim * len(self.base)
 
 
-Alphabet = Union[ExplicitAlphabet, ProductAlphabet, SupAlphabet]
+Alphabet = Union[
+    ExplicitAlphabet,
+    ExponentialAlphabet,
+    ProductAlphabet,
+    SupAlphabet,
+]
