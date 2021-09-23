7b94a3254d5169297e52ecccae4010c61b79a39a
Marcell Vazquez-Chanlatte <mvc@linux.com>
2021-09-23 14:19:17 -0700

    Expose internal types.

--- dfa/__init__.py
+++ dfa/__init__.py
@@ -1,3 +1,12 @@
 # flake8: noqa
-from dfa.dfa import DFA
+from dfa.dfa import DFA, State, Letter, Alphabet
 from dfa.utils import dfa2dict, dict2dfa
+
+__all__ = [
+    'Alphabet',
+    'DFA',
+    'Letter',
+    'State',
+    'dfa2dict',
+    'dict2dfa',
+]
